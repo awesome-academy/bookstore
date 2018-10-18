34401e416dd6d8e6b7dbb1f221ab3dd5ebac78d5
hoangtronghieu <hoangtronghieu1812@gmail.com>
2018-10-18 17:18:17 +0700

    Them page Blog: cho ticket booknews

--- db/schema.rb
+++ db/schema.rb
@@ -26,13 +26,23 @@ ActiveRecord::Schema.define(version: 2018_10_23_084907) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "date"
+    t.string "description"
+    t.string "auth_link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "publisher"
     t.float "price"
     t.integer "quantity_in_store"
     t.string "image"
-    t.text "desciption"
+    t.text "description"
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

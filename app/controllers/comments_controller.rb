cfa20b344fc4b9d55d3536ef696e8f4b338b373d
duongthao1610 <duongthao.1610@gmail.com>
2018-11-07 08:40:49 +0700

    user delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,12 +7,24 @@ class CommentsController < ApplicationController
     @book = Book.find_by id: params[:book_id]
     @comment = @book.comments.build comment_params
     @comment.user_id = current_user.id
-
     if @comment.save
       render json: {
         status: :success,
         content: render_to_string(partial: "comments/comment", locals:{comment: @comment})
-        }
+      }
+    end
+  end
+
+  def destroy
+    @book = Book.find_by id: params[:book_id]
+    @comment = Comment.find_by id: params[:id]
+
+    unless @comment.destroy
+      flash[:danger] = t ".deleted_fail"
+    end
+    respond_to do |format|
+      format.html { redirect_to @book }
+      format.js
     end
   end
 
